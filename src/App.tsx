b4c6e6f8d1230d32b5362e3e7121db732e035999
shreerai001 <shreerai1@outlook.com>
2021-12-31 23:44:01 +0545

    cloud run CI random test

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Sigma news google cloud run
+          Sigma news google cloud run, react ts
         </p>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
